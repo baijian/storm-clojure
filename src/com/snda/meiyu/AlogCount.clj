6542e044983ea236c0163a93a949bfc92acf9b71
柏健 <jian.baij@gmail.com>
2013-11-05 14:39:21 +0800

    add some code of alogcount topology

--- src/com/snda/meiyu/AlogCount.clj
+++ src/com/snda/meiyu/AlogCount.clj
@@ -1,7 +1,10 @@
 (ns com.snda.meiyu.AlogCount
-  (:import [backtype.storm StormSubmitter LocalCluster])
-  (:use [backtype.storm clojure config])
-  (:use [clojure.tools.logging :only (info debug error)])
+  (:import 
+    '(backtype.storm StormSubmitter LocalCluster)
+    '(backtype.storm.topology IRichSpout)
+    '(backtype.storm.topology IRichBolt))
+  (:use '[backtype.storm clojure config])
+  (:use '[clojure.tools.logging :only (info debug error)])
   (:gen-class))
 
 (defspout alogSpout)
@@ -9,6 +12,30 @@
 (defbolt filterUriBolt)
 
 (defbolt countBolt)
+
+(defn mk-topology []
+  (topology
+    {"1" (spout-spec alogSpout)
+     "2" (bolt-spec {}
+                    filterUriBolt
+                    :p 5)
+     "3" (bolt-spec {}
+                    countBolt
+                    :p 6)}))
+
+(defn run-local! []
+  (let [cluster (LocalCluster.)]
+    (.submitTopology cluster "AlogCount" {TOPOLOGY-DEBUG true} (mk-topology))
+    (Thread/sleep 10000)
+    (.shutdown cluster)
+    ))
+
+(defn submit-topology! [name]
+  (StormSubmitter/submitTopology
+    name
+    {TOPOLOGY-DEBUG true
+     TOPOLOGY-WORKERS 3}
+    (mk-topology)))
 
 (defn -main
   ([]
